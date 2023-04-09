61156682570439e41c63fab704e37b25f5cb9aac
Baljinnyam Dayan <baljinnyam.dayan@gmail.com>
2023-04-09 17:00:10 +0800

    init melmy

---
+++ pages/api/imagepredict/index.ts
@@ -0,0 +1,40 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import Replicate from "replicate";
+import fs from "fs";
+import axios from "axios";
+import { translate } from "bing-translate-api";
+
+const replicate = new Replicate({
+	auth: process.env.REPLICATE_API_TOKEN ?? "",
+});
+
+export default async function hander(req: NextApiRequest, res: NextApiResponse) {
+	const output = await replicate.run(
+		"salesforce/blip:2e1dddc8621f72155f24cf2e0adbde548458d3cab9f00c0139eea840d0ac4746",
+		{
+			input: {
+				image: req.body,
+			},
+		}
+	);
+	const caption = output.split(":")[1];
+
+	const translation = await translate(caption, "en", "mn-Cyrl");
+
+	const url = "https://api.chimege.com/v1.2/synthesize";
+	const headers = {
+		"Content-Type": "plain/text",
+		Token: process.env.CHIMEGE_TOKEN,
+	};
+	const text = "Зурагны тайлбар бол : " + translation.translation;
+
+	const response = await axios.post(url, text, { headers, responseType: "arraybuffer" });
+
+	const buffer = Buffer.from(response.data);
+
+	res.setHeader("Content-Type", "audio/wav");
+	res.setHeader("Content-Length", buffer.length);
+	res.setHeader("Content-Disposition", 'attachment; filename="hello.wav"');
+	res.write(buffer);
+	res.end();
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,124 +1,315 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { Dots } from "@/components/dots";
+import {
+	Button,
+	Container,
+	Flex,
+	Group,
+	Image as MantineImage,
+	Modal,
+	Text,
+	Title,
+	createStyles,
+	rem,
+} from "@mantine/core";
+import { Dropzone, MIME_TYPES } from "@mantine/dropzone";
+import { useDisclosure } from "@mantine/hooks";
+import { IconCamera, IconCloudUpload, IconDownload, IconPaperclip, IconX } from "@tabler/icons-react";
+import { useRef, useState } from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+const useStyles = createStyles((theme) => ({
+	wrapper: {
+		position: "relative",
+		paddingTop: rem(120),
+		paddingBottom: rem(80),
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+		[theme.fn.smallerThan("sm")]: {
+			paddingTop: rem(80),
+			paddingBottom: rem(60),
+		},
+	},
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+	inner: {
+		position: "relative",
+		zIndex: 1,
+	},
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+	dots: {
+		position: "absolute",
+		color: theme.colorScheme === "dark" ? theme.colors.dark[5] : theme.colors.gray[1],
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+		[theme.fn.smallerThan("sm")]: {
+			display: "none",
+		},
+	},
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
+	dotsLeft: {
+		left: 0,
+		top: 0,
+	},
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+	title: {
+		textAlign: "center",
+		fontWeight: 800,
+		fontSize: rem(40),
+		letterSpacing: -1,
+		color: theme.colorScheme === "dark" ? theme.white : theme.black,
+		marginBottom: theme.spacing.xs,
+		fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+
+		[theme.fn.smallerThan("xs")]: {
+			fontSize: rem(28),
+			textAlign: "left",
+		},
+	},
+
+	highlight: {
+		background: "linear-gradient(to right, #228be6, #ff007f)",
+		WebkitBackgroundClip: "text",
+		WebkitTextFillColor: "transparent",
+	},
+
+	description: {
+		textAlign: "center",
+
+		[theme.fn.smallerThan("xs")]: {
+			textAlign: "left",
+			fontSize: theme.fontSizes.md,
+		},
+	},
+
+	controls: {
+		marginTop: theme.spacing.lg,
+		display: "flex",
+		justifyContent: "center",
+
+		[theme.fn.smallerThan("xs")]: {
+			flexDirection: "column",
+		},
+	},
+
+	control: {
+		"&:not(:first-of-type)": {
+			marginLeft: theme.spacing.md,
+		},
+
+		[theme.fn.smallerThan("xs")]: {
+			height: rem(42),
+			fontSize: theme.fontSizes.md,
+
+			"&:not(:first-of-type)": {
+				marginTop: theme.spacing.md,
+				marginLeft: 0,
+			},
+		},
+	},
+
+	wrapperD: {
+		position: "relative",
+		marginBottom: rem(30),
+		marginTop: rem(60),
+	},
+
+	dropzone: {
+		borderWidth: rem(1),
+		paddingBottom: rem(50),
+	},
+
+	icon: {
+		color: theme.colorScheme === "dark" ? theme.colors.dark[3] : theme.colors.gray[4],
+	},
+
+	controlD: {
+		position: "absolute",
+		width: rem(250),
+		left: `calc(50% - ${rem(180)})`,
+		bottom: rem(-20),
+	},
+}));
+
+async function sendImage(imageData: File, setImageDetail: any, setAudioUrl: any) {
+	if (!imageData) return;
+	// compress imageData
+	const compressed = await compressImage(imageData);
+
+	const reader = new FileReader();
+	reader.readAsDataURL(compressed);
+	reader.onload = async () => {
+		const response = await fetch("/api/imagepredict", {
+			method: "POST",
+			body: reader.result,
+		});
+		const blob = await response.blob();
+		const url = URL.createObjectURL(blob);
+		setAudioUrl(url);
+	};
+	reader.onerror = (error) => {
+		console.log("Error: ", error);
+	};
+	return "error";
 }
+
+// get token from env
+
+async function compressImage(imageData: File) {
+	return new Promise((resolve, reject) => {
+		const canvas = document.createElement("canvas");
+		const ctx = canvas.getContext("2d");
+		const img = new Image();
+
+		img.onload = () => {
+			const MAX_WIDTH = 800;
+			const MAX_HEIGHT = 600;
+			let width = img.width;
+			let height = img.height;
+
+			if (width > height) {
+				if (width > MAX_WIDTH) {
+					height *= MAX_WIDTH / width;
+					width = MAX_WIDTH;
+				}
+			} else {
+				if (height > MAX_HEIGHT) {
+					width *= MAX_HEIGHT / height;
+					height = MAX_HEIGHT;
+				}
+			}
+
+			canvas.width = width;
+			canvas.height = height;
+
+			ctx.drawImage(img, 0, 0, width, height);
+
+			canvas.toBlob(
+				(blob) => {
+					resolve(new File([blob], "compressed.jpg", { type: "image/jpeg" }));
+				},
+				"image/jpeg",
+				0.8
+			); // Set the JPEG quality to 80%
+		};
+
+		img.onerror = (error) => reject(error);
+
+		img.src = URL.createObjectURL(imageData);
+	});
+}
+
+const Home = () => {
+	const [audioUrl, setAudioUrl] = useState(null);
+	const { classes, theme } = useStyles();
+	const openRef = useRef<() => void>(null);
+	const [images, setImages] = useState<any[]>();
+	const [objectData, setObjectData] = useState<any>();
+	const [opened, { open, close }] = useDisclosure(false);
+	const [imageDetail, setImageDetail] = useState<string>("");
+
+	const previews = images?.map((file, index) => {
+		const imageUrl = URL.createObjectURL(file || "");
+		return (
+			<MantineImage
+				alt="image"
+				key={index}
+				src={imageUrl}
+				imageProps={{ onLoad: () => URL.revokeObjectURL(imageUrl) }}
+			/>
+		);
+	});
+
+	return (
+		<Container className={classes.wrapper} size={1400}>
+			<Dots className={classes.dots} style={{ left: 0, top: 0 }} />
+			<Dots className={classes.dots} style={{ left: 60, top: 0 }} />
+			<Dots className={classes.dots} style={{ left: 0, top: 140 }} />
+			<Dots className={classes.dots} style={{ right: 0, top: 60 }} />
+
+			<div className={classes.inner}>
+				<Title className={classes.title}>
+					<Text component="span" className={classes.highlight} inherit>
+						Melmy
+					</Text>{" "}
+					- Зураг танигч систем
+				</Title>
+
+				<Container p={0} size={600}>
+					<Text size="lg" color="dimmed" className={classes.description}>
+						Машин сургалт ашиглан зурагнаас обьект таньж аудио хэлбэрээр уншиж өгөх систем
+					</Text>
+				</Container>
+
+				<div className={classes.wrapperD}>
+					<Dropzone
+						openRef={openRef}
+						onDrop={(files) => {
+							setImages(files);
+							open();
+						}}
+						className={classes.dropzone}
+						radius="md"
+						accept={[MIME_TYPES.jpeg, MIME_TYPES.png]}
+						// maxSize={2 * 1024}
+						maxFiles={1}
+					>
+						{/* <div style={{ pointerEvents: "none" }}> */}
+						<Group position="center">
+							<Dropzone.Accept>
+								<IconDownload size={rem(50)} color={theme.colors[theme.primaryColor][6]} stroke={1.5} />
+							</Dropzone.Accept>
+							<Dropzone.Reject>
+								<IconX size={rem(50)} color={theme.colors.red[6]} stroke={1.5} />
+							</Dropzone.Reject>
+							<Dropzone.Idle>
+								<IconCloudUpload
+									size={rem(50)}
+									color={theme.colorScheme === "dark" ? theme.colors.dark[0] : theme.black}
+									stroke={1.5}
+								/>
+							</Dropzone.Idle>
+						</Group>
+
+						<Text ta="center" fw={700} fz="lg" mt="xl">
+							<Dropzone.Accept>Энд зөөнө үү</Dropzone.Accept>
+							<Dropzone.Reject>Зураг биш байна</Dropzone.Reject>
+							<Dropzone.Idle>Зураг оруулах</Dropzone.Idle>
+						</Text>
+						<Text ta="center" fz="sm" mt="xs" c="dimmed">
+							Оруулах зурагаа ийшээ зөөж авчирна уу?
+							<i>.jpg, .png</i> өргөтгөлтэй 2mb-с бага зургаа оруулна уу
+						</Text>
+						{/* </div> */}
+					</Dropzone>
+					<Flex className={classes.controlD} gap={16}>
+						<Button size="md" radius="xl" onClick={() => openRef.current?.()} rightIcon={<IconPaperclip />}>
+							Зураг сонгох
+						</Button>
+						<Button
+							// className={classes.controlD}
+							size="md"
+							radius="xl"
+							onClick={() => openRef.current?.()}
+							rightIcon={<IconCamera />}
+						>
+							Камер нээх
+						</Button>
+					</Flex>
+				</div>
+				<Modal opened={opened} onClose={close} title="Сонгогдсон зураг">
+					{previews}
+					<Button
+						onClick={() => {
+							sendImage(images?.[0], setImageDetail, setAudioUrl);
+						}}
+						w={"100%"}
+						mt={12}
+					>
+						Илгээх
+					</Button>
+				</Modal>
+				<div className={classes.wrapper}>{imageDetail}</div>
+			</div>
+			{/* <Button onClick={() => {}}>FUCK YOIU</Button> */}
+			<div>{audioUrl && <audio controls autoPlay src={audioUrl} />}</div>
+		</Container>
+	);
+};
+
+export default Home;
